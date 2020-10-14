81b7248bec474e0f94139c9c6f3934cb33e6c197
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-14 14:16:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,17 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {}
-      :monarch => {}
-      :hero => {}
-      :hero_friends =>{}
+      :patriarch => {},
+      :monarch => {},
+      :hero => {},
+      :hero_friends =>[]
       }, 
     :capulet => {
       :patriarch => {}
       :monarch => {}
       :hero => {}
-      :hero_friends =>{}
-      }}
+      :heroine_friends =>[]
+      }
+  }
 
   
 end
