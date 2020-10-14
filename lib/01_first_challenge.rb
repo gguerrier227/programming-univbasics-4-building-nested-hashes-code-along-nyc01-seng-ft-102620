5c75d17b735b303c9d7774ad43735c5850f88dc2
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-14 14:06:18 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,9 +1,11 @@
 def first_challenge
-   epic_tragedy = {
+   epic_tragedy = [
      :montague => {
        :patriarch => {
          :name => "Lord Montague"
          :age => "53"
        }
      }
+  ]
+     
 end
